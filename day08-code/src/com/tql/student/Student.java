c8bdbe139b53942a8692fa53d0db6c6c573cdff2
tql <tqlttql@gmail.com>
2022-09-13 19:31:40 +0800

    Changes

--- day08-code/src/com/tql/student/Student.java
+++ day08-code/src/com/tql/student/Student.java
@@ -1,5 +1,33 @@
 package com.tql.student;
 
 public class Student {
+    private String name;
+    private int age;
 
+    //如果没有写任何构造方法
+    //虚拟机会自己加一个空构造方法
+
+    public Student(){
+        //空参构造
+        System.out.println("看看我执行了吗");
+    }
+
+    public Student(String name,int age){
+        //带参构造--对成员变量赋值
+        this.name =name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age =age;
+    }
 }
---
+++ day08-code/src/com/tql/student/StudentTest.java
@@ -0,0 +1,11 @@
+package com.tql.student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        //使用带参构造
+        Student s = new Student("tql",22);
+        System.out.println(s.getAge());
+        System.out.println(s.getName());
+
+    }
+}
